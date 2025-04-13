e115f67cb226ff285736ed4612c41079d5a2a1bf
Jackpot-007 <jackpotmerlon@gmail.com>
2025-04-13 18:43:37 +0000

    binary_trees 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -11,12 +12,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	/* Reservar memoria para el nuevo nodo */
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-	return (NULL);
+		return (NULL);
 
-	/* Inicializar los campos del nodo */
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
